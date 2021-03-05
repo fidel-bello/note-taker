b86407fc3bc50bccde57a05fd217e3896c2e3203
fidel bello <59423@bears.smcc.edu>
2021-03-05 15:23:59 -0500

    fixed issue where app was not defined

--- Develop/routes/hmtl.js
+++ Develop/routes/hmtl.js
@@ -2,6 +2,8 @@
 const path = require('path');
  
 //get requet for html
-module.exports = function(app) {app.get('/notes', function(req, res){res.sendFile(path.join(__dirname, '../public/notes.html'))})};
+module.exports = function(app) {
+    app.get('/notes', function(req, res){res.sendFile(path.join(__dirname, '../public/notes.html'))});
 
 app.get('*', function(req,res){res.sendFile(path.join(__dirname, '../public/indec.html'))});
+};
