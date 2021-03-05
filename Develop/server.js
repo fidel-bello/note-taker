dd0bf38b306c0c1a30c52b3af1eadfd23ab32096
fidel bello <59423@bears.smcc.edu>
2021-03-05 12:45:31 -0500

    added more modules

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,10 @@
 // node modules
 const express = require('express');
 const fs = require('fs');
-
-const app = express()
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 8080;
+const archive = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
 //routes
 app.get('/notes', function (req, res){res.sendFile(path.join(__dirname, './public/notes.html'))});
 
