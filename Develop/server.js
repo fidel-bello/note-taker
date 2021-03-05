dbaf1e083ac87eb2ccb5a96877f2264dd901ba8a
fidel bello <59423@bears.smcc.edu>
2021-03-05 12:38:19 -0500

    added basic routes to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,3 +1,9 @@
 // node modules
-const expresss = require('express');
+const express = require('express');
 const fs = require('fs');
+
+const app = express()
+//routes
+app.get('/notes', function (req, res){res.sendFile(path.join(__dirname, './public/notes.html'))});
+
+app.get('/api/notes', function (req,res){res.sendFile(path.join(__dirname, './db/db.json'))});
