5b1a7b092c13d7a2f7c00dff3cb9f748ad427421
fidel bello <59423@bears.smcc.edu>
2021-03-05 15:00:51 -0500

    copied and pasted same code but took out the api routes

---
+++ Develop/server.js
@@ -0,0 +1,17 @@
+// node modules
+const express = require('express');
+
+// express server
+const app = express()
+
+//port
+const PORT = process.env.PORT || 3000;
+
+//express data parsing
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+//app listener
+app.listen(PORT, () => console.log(`App is listening on Port : ${PORT}`));
+
