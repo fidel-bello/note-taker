efa6442234a7be8b52cd1458519c30e893266d1d
fidel bello <59423@bears.smcc.edu>
2021-03-05 20:46:24 -0500

    successfull function on posting note and  save

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -6,5 +6,16 @@ module.exports = function (app) {
     let postNote = data.length;
     app.get('/api/notes', function (req, res){
         res.json(data)
+    });
+
+    app.post('/api/notes', function (req, res){
+        let savedNote = req.body;
+        let id = '' + postNote;
+        savedNote.id = id;
+        postNote = postNote + 1;
+        data.push(savedNote);
+        fs.writeFile('./db/db.s\json', JSON.stringify(data), () => {
+            console.log('note successful')});
+            res.json(savedNote);
     })
 }
