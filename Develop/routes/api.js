1e9323ced6278c39679fdebf24fe6754fa5ac27b
fidel bello <59423@bears.smcc.edu>
2021-03-05 21:00:28 -0500

    successful function to delete note

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -7,15 +7,42 @@ module.exports = function (app) {
     app.get('/api/notes', function (req, res){
         res.json(data)
     });
+// function adds and saves notes to server and logs 'note succesfull'
 
     app.post('/api/notes', function (req, res){
+        
         let savedNote = req.body;
+        
         let id = '' + postNote;
+        
         savedNote.id = id;
+        
         postNote = postNote + 1;
+        
         data.push(savedNote);
+        
         fs.writeFile('./db/db.s\json', JSON.stringify(data), () => {
+            
             console.log('note successful')});
+           
             res.json(savedNote);
+    });
+
+
+    // function to delete notes
+    app.delete('/api/notes/:id', function (req, res){
+        let selected = req.params.id;
+
+        for (let index = 0; index < data.length; index++) {
+          if (selected === data[index].id) {
+              data.splice(index, 1);
+              fs.writeFile('./db/db.json', JSON.stringify(data),() => {
+                  console.log('deleted!')});
+          };
+            
+        };
+    res.json(data)
     })
+
+
 }
